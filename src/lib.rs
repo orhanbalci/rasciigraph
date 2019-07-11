15c17c32395033d7ca5bcddfc30a6a1b3fba922e
orhanbalci@gmail.com <orhanbalci@gmail.com>
2019-07-12 00:18:38 +0300

    all test cases are added errors reported from test cases are fixed

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,28 @@ pub struct Config {
     caption: String,
 }
 
+impl Config {
+    fn with_caption(mut self, caption: String) -> Self {
+        self.caption = caption;
+        self
+    }
+
+    fn with_height(mut self, height: u32) -> Self {
+        self.height = height;
+        self
+    }
+
+    fn with_width(mut self, width: u32) -> Self {
+        self.width = width;
+        self
+    }
+
+    fn with_offset(mut self, offset: u32) -> Self {
+        self.offset = offset;
+        self
+    }
+}
+
 pub fn plot(series: Vec<f64>, mut config: Config) -> String {
     let series_inner = if config.width > 0 {
         interpolate(series, config.width)
@@ -19,8 +41,9 @@ pub fn plot(series: Vec<f64>, mut config: Config) -> String {
 
     let interval = (max - min).abs();
     if config.height == 0 {
-        if interval as i32 <= 0 {
-            config.height = (interval as i32 * 10i32.pow(-interval.log10().ceil() as u32)) as u32;
+        if interval <= 1f64 {
+            config.height =
+                (interval * f64::from(10i32.pow((-interval.log10()).ceil() as u32))) as u32;
         } else {
             config.height = interval as u32;
         }
@@ -181,38 +204,24 @@ fn min_max(series: &[f64]) -> (f64, f64) {
 #[cfg(test)]
 mod tests {
 
-    #[test]
-    fn test_min_max() {
-        assert_eq!(
-            (-2f64, 11f64),
-            crate::min_max(&vec![
-                2f64, 1f64, 1f64, 2f64, -2f64, 5f64, 7f64, 11f64, 3f64, 7f64, 1f64
-            ])
-        );
-    }
-    #[test]
-    fn test_ones() {
-        let res = crate::plot(vec![1f64, 1f64, 1f64, 1f64, 1f64], crate::Config::default());
-        assert_eq!(res, " 1.00 ┼────");
-    }
-
-    #[test]
-    fn test_zeros() {
-        let res = crate::plot(vec![0f64, 0f64, 0f64, 0f64, 0f64], crate::Config::default());
-        assert_eq!(res, " 0.00 ┼────");
+    macro_rules! graph_eq {
+        ($fname:ident ? [$($series:expr),*]  => $rhs:expr) => {
+            #[test]
+            fn $fname(){
+              let res = crate::plot(vec![$(f64::from($series),)*], crate::Config::default());
+              assert_eq!(res, $rhs);
+        }};
+        ($fname:ident ? [$($series:expr),*]  ? $config:expr => $rhs:expr) => {
+            #[test]
+            fn $fname(){
+              let res = crate::plot(vec![$(f64::from($series),)*], $config);
+              assert_eq!(res, $rhs);
+        }};
     }
 
-    #[test]
-    fn third_test() {
-        let res = crate::plot(
-            vec![
-                2f64, 1f64, 1f64, 2f64, -2f64, 5f64, 7f64, 11f64, 3f64, 7f64, 1f64,
-            ],
-            crate::Config::default(),
-        );
-        assert_eq!(
-            res,
-            " 11.00 ┤      ╭╮   
+    graph_eq!(test_ones  ? [1, 1, 1, 1, 1] => " 1.00 ┼────");
+    graph_eq!(test_zeros ? [0, 0, 0, 0, 0] => " 0.00 ┼────");
+    graph_eq!(test_three ? [2, 1, 1, 2, -2, 5, 7, 11, 3, 7, 1] => " 11.00 ┤      ╭╮   
  10.00 ┤      ││   
   9.00 ┼      ││   
   8.00 ┤      ││   
@@ -225,28 +234,11 @@ mod tests {
   1.00 ┤╰─╯││    ╰ 
   0.00 ┤   ││      
  -1.00 ┤   ││      
- -2.00 ┤   ╰╯     "
-        );
-    }
+ -2.00 ┤   ╰╯     ");
 
-    #[test]
-    fn fourth_test() {
-        let res = crate::plot(
-            vec![
-                2f64, 1f64, 1f64, 2f64, -2f64, 5f64, 7f64, 11f64, 3f64, 7f64, 4f64, 5f64, 6f64,
-                9f64, 4f64, 0f64, 6f64, 1f64, 5f64, 3f64, 6f64, 2f64,
-            ],
-            crate::Config {
-                width: 0,
-                height: 0,
-                offset: 0,
-                caption: "Plot using asciigraph.".to_string(),
-            },
-        );
-
-        assert_eq!(
-            res,
-            " 11.00 ┤      ╭╮              
+    graph_eq!(test_four ? [2, 1, 1, 2, -2, 5, 7, 11, 3, 7, 4, 5, 6, 9, 4, 0, 6, 1, 5, 3, 6, 2] ? 
+    crate::Config::default().with_caption("Plot using asciigraph.".to_string()) 
+    => " 11.00 ┤      ╭╮              
  10.00 ┤      ││              
   9.00 ┼      ││    ╭╮        
   8.00 ┤      ││    ││        
@@ -260,7 +252,169 @@ mod tests {
   0.00 ┤   ││         ╰╯      
  -1.00 ┤   ││                 
  -2.00 ┤   ╰╯                
-          Plot using asciigraph."
-        )
+          Plot using asciigraph.");
+
+    graph_eq!(test_five ? [ 2, 1, 1, 2, -2, 5, 7, 11, 3, 7, 4, 5, 6, 9, 4, 0, 6, 1, 5, 3, 6, 2] ? 
+                crate::Config::default().with_caption("Plot using asciigraph.".to_string()) 
+     => " 11.00 ┤      ╭╮              
+ 10.00 ┤      ││              
+  9.00 ┼      ││    ╭╮        
+  8.00 ┤      ││    ││        
+  7.00 ┤     ╭╯│╭╮  ││        
+  6.00 ┤     │ │││ ╭╯│ ╭╮  ╭╮ 
+  5.00 ┤    ╭╯ │││╭╯ │ ││╭╮││ 
+  4.00 ┤    │  ││╰╯  ╰╮││││││ 
+  3.00 ┤    │  ╰╯     ││││╰╯│ 
+  2.00 ┼╮ ╭╮│         ││││  ╰ 
+  1.00 ┤╰─╯││         ││╰╯    
+  0.00 ┤   ││         ╰╯      
+ -1.00 ┤   ││                 
+ -2.00 ┤   ╰╯                
+          Plot using asciigraph." );
+
+    graph_eq!(test_six ? [0.2, 0.1, 0.2, 2, -0.9, 0.7, 0.91, 0.3, 0.7, 0.4, 0.5] ? 
+    crate::Config::default().with_caption("Plot using asciigraph.".to_string())
+    => "  2.00 ┤  ╭╮ ╭╮    
+  0.55 ┼──╯│╭╯╰─── 
+ -0.90 ┤   ╰╯     
+          Plot using asciigraph." );
+
+    graph_eq!(test_seven ? [2, 1, 1, 2, -2, 5, 7, 11, 3, 7, 1] ? 
+    crate::Config::default().with_height(4).with_offset(3)
+    => " 11.00 ┤      ╭╮   
+  7.75 ┼    ╭─╯│╭╮ 
+  4.50 ┼╮ ╭╮│  ╰╯│ 
+  1.25 ┤╰─╯││    ╰ 
+ -2.00 ┤   ╰╯     "
+    );
+
+    graph_eq!(test_eight ? [0.453, 0.141, 0.951, 0.251, 0.223, 0.581, 0.771, 0.191, 0.393, 0.617, 0.478]
+    => " 0.95 ┤ ╭╮        
+ 0.85 ┤ ││  ╭╮    
+ 0.75 ┤ ││  ││    
+ 0.65 ┤ ││ ╭╯│ ╭╮ 
+ 0.55 ┤ ││ │ │ │╰ 
+ 0.44 ┼╮││ │ │╭╯  
+ 0.34 ┤│││ │ ││   
+ 0.24 ┤││╰─╯ ╰╯   
+ 0.14 ┤╰╯        ");
+
+    graph_eq!(test_nine ? [0.01, 0.004, 0.003, 0.0042, 0.0083, 0.0033, 0.0079] 
+    => " 0.010 ┼╮      
+ 0.009 ┤│      
+ 0.008 ┤│  ╭╮╭ 
+ 0.007 ┤│  │││ 
+ 0.006 ┤│  │││ 
+ 0.005 ┤│  │││ 
+ 0.004 ┤╰╮╭╯││ 
+ 0.003 ┤ ╰╯ ╰╯"
+    );
+
+    graph_eq!(test_ten ? [192, 431, 112, 449, -122, 375, 782, 123, 911, 1711, 172] ? crate::Config::default().with_height(10)
+    => " 1711 ┤        ╭╮ 
+ 1528 ┼        ││ 
+ 1344 ┤        ││ 
+ 1161 ┤        ││ 
+  978 ┤       ╭╯│ 
+  794 ┤     ╭╮│ │ 
+  611 ┤     │││ │ 
+  428 ┤╭╮╭╮╭╯││ │ 
+  245 ┼╯╰╯││ ╰╯ ╰ 
+   61 ┤   ││      
+ -122 ┤   ╰╯     ");
+
+    graph_eq!(test_eleven ? [0.3189989805, 0.149949026, 0.30142492354, 0.195129182935, 0.3142492354, 
+    0.1674974513, 0.3142492354, 0.1474974513, 0.3047974513] ?
+    crate::Config::default().with_width(30).with_height(5).with_caption("Plot with custom height & width.".to_string())
+        => " 0.32 ┼╮            ╭─╮     ╭╮     ╭ 
+ 0.29 ┤╰╮    ╭─╮   ╭╯ │    ╭╯│     │ 
+ 0.26 ┤ │   ╭╯ ╰╮ ╭╯  ╰╮  ╭╯ ╰╮   ╭╯ 
+ 0.23 ┤ ╰╮ ╭╯   ╰╮│    ╰╮╭╯   ╰╮ ╭╯  
+ 0.20 ┤  ╰╮│     ╰╯     ╰╯     │╭╯   
+ 0.16 ┤   ╰╯                   ╰╯   
+         Plot with custom height & width."
+    );
+
+    graph_eq!(test_twelve ? [0, 0, 0, 0, 1.5, 0, 0, -0.5, 9, -3, 0, 0, 1, 2, 1, 0, 0, 0, 0,
+				0, 0, 0, 0, 1.5, 0, 0, -0.5, 8, -3, 0, 0, 1, 2, 1, 0, 0, 0, 0,
+				0, 0, 0, 0, 1.5, 0, 0, -0.5, 10, -3, 0, 0, 1, 2, 1, 0, 0, 0, 0] ? 
+                crate::Config::default().with_offset(10).with_height(10).with_caption("I'm a doctor, not an engineer.".to_string())
+    => "     10.00    ┤                                             ╭╮          
+      8.70    ┤       ╭╮                                    ││          
+      7.40    ┼       ││                 ╭╮                 ││          
+      6.10    ┤       ││                 ││                 ││          
+      4.80    ┤       ││                 ││                 ││          
+      3.50    ┤       ││                 ││                 ││          
+      2.20    ┤       ││   ╭╮            ││   ╭╮            ││   ╭╮     
+      0.90    ┤   ╭╮  ││  ╭╯╰╮       ╭╮  ││  ╭╯╰╮       ╭╮  ││  ╭╯╰╮    
+     -0.40    ┼───╯╰──╯│╭─╯  ╰───────╯╰──╯│╭─╯  ╰───────╯╰──╯│╭─╯  ╰─── 
+     -1.70    ┤        ││                 ││                 ││         
+     -3.00    ┤        ╰╯                 ╰╯                 ╰╯        
+                 I'm a doctor, not an engineer.");
+
+    graph_eq!(test_thirteen ? [-5, -2, -3, -4, 0, -5, -6, -7, -8, 0, -9, -3, -5, -2, -9, -3, -1]
+    => "  0.00 ┤   ╭╮   ╭╮       
+ -1.00 ┤   ││   ││     ╭ 
+ -2.00 ┤╭╮ ││   ││  ╭╮ │ 
+ -3.00 ┤│╰╮││   ││╭╮││╭╯ 
+ -4.00 ┤│ ╰╯│   │││││││  
+ -5.00 ┼╯   ╰╮  │││╰╯││  
+ -6.00 ┤     ╰╮ │││  ││  
+ -7.00 ┤      ╰╮│││  ││  
+ -8.00 ┤       ╰╯││  ││  
+ -9.00 ┼         ╰╯  ╰╯ ");
+
+    graph_eq!(test_fourteen ? [-0.000018527, -0.021, -0.00123, 0.00000021312, 
+    -0.0434321234, -0.032413241234, 0.0000234234] ?
+        crate::Config::default().with_height(5).with_width(45)
+        => "  0.000 ┼─╮           ╭────────╮                    ╭ 
+ -0.008 ┤ ╰──╮     ╭──╯        ╰─╮                ╭─╯ 
+ -0.017 ┤    ╰─────╯             ╰╮             ╭─╯   
+ -0.025 ┤                         ╰─╮         ╭─╯     
+ -0.034 ┤                           ╰╮   ╭────╯       
+ -0.042 ┼                            ╰───╯           "
+    );
+
+    graph_eq!(test_fifteen ? [57.76, 54.04, 56.31, 57.02, 59.5, 52.63, 52.97, 56.44, 56.75, 52.96, 55.54, 
+    55.09, 58.22, 56.85, 60.61, 59.62, 59.73, 59.93, 56.3, 54.69, 55.32, 54.03, 50.98, 50.48, 54.55, 47.49, 
+    55.3, 46.74, 46, 45.8, 49.6, 48.83, 47.64, 46.61, 54.72, 42.77, 50.3, 42.79, 41.84, 44.19, 43.36, 45.62, 
+    45.09, 44.95, 50.36, 47.21, 47.77, 52.04, 47.46, 44.19, 47.22, 45.55, 40.65, 39.64, 37.26, 40.71, 42.15, 
+    36.45, 39.14, 36.62]
+   => " 60.61 ┤             ╭╮ ╭╮                                          
+ 59.60 ┤   ╭╮        │╰─╯│                                          
+ 58.60 ┤   ││      ╭╮│   │                                          
+ 57.59 ┼╮ ╭╯│      │││   │                                          
+ 56.58 ┤│╭╯ │ ╭─╮  │╰╯   ╰╮                                         
+ 55.58 ┤││  │ │ │╭─╯      │╭╮    ╭╮                                 
+ 54.57 ┤╰╯  │ │ ││        ╰╯╰╮ ╭╮││      ╭╮                         
+ 53.56 ┤    │╭╯ ╰╯           │ ││││      ││                         
+ 52.56 ┤    ╰╯               │ ││││      ││           ╭╮            
+ 51.55 ┤                     ╰╮││││      ││           ││            
+ 50.54 ┤                      ╰╯│││      ││╭╮      ╭╮ ││            
+ 49.54 ┤                        │││  ╭─╮ ││││      ││ ││            
+ 48.53 ┤                        │││  │ │ ││││      ││ ││            
+ 47.52 ┤                        ╰╯│  │ ╰╮││││      │╰─╯╰╮╭╮         
+ 46.52 ┤                          ╰─╮│  ╰╯│││      │    │││         
+ 45.51 ┤                            ╰╯    │││   ╭──╯    ││╰╮        
+ 44.50 ┤                                  │││ ╭╮│       ╰╯ │        
+ 43.50 ┤                                  ││╰╮│╰╯          │        
+ 42.49 ┤                                  ╰╯ ╰╯            │   ╭╮   
+ 41.48 ┤                                                   │   ││   
+ 40.48 ┤                                                   ╰╮ ╭╯│   
+ 39.47 ┤                                                    ╰╮│ │╭╮ 
+ 38.46 ┤                                                     ││ │││ 
+ 37.46 ┤                                                     ╰╯ │││ 
+ 36.45 ┤                                                        ╰╯╰"
+    );
+
+    #[test]
+    fn test_min_max() {
+        assert_eq!(
+            (-2f64, 11f64),
+            crate::min_max(&vec![
+                2f64, 1f64, 1f64, 2f64, -2f64, 5f64, 7f64, 11f64, 3f64, 7f64, 1f64
+            ])
+        );
     }
+
 }
